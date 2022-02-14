b7cb66b1a2681140fe40b91f4c45d6c9fabee978
MVRKKSK <82573421+MVRKKSK@users.noreply.github.com>
2022-02-14 21:57:30 +0530

    Add files via upload

---
+++ pages/Preview2.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Preview2 from "../components/preview2/Preview2"
+
+const Preview = () => {
+  return (
+    <div><Preview2 /></div>
+  )
+}
+
+export default Preview
