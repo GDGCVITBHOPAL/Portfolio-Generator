539d186a3280b5f7db4c2f45777defb5640100c9
MVRKKSK <82573421+MVRKKSK@users.noreply.github.com>
2022-02-14 22:01:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    domains: ['encodedbicoding.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
   reactStrictMode: true,
 }
